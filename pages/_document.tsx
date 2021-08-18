02e4dc4aee9dec8ffcb63fd45d1c9661941908b5
tarasov <tarasov@reksoft.ru>
2021-08-19 02:23:45 +0300

    NextJS-chapter-7 config locale

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
     render(): JSX.Element {
         return (
-            <Html lang={'ru'}>
+            <Html>
                 <Head/>
                 <body>
                     <Main />
