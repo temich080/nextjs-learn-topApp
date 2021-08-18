e57df5b533b2d45c831fd72395e4992a358987cc
tarasov <tarasov@reksoft.ru>
2021-08-19 02:11:42 +0300

    NextJS-chapter-7 config locale

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
+  i18n: {
+    localeDetection: true,
+    locales: ['ru', 'en', 'catchAll'],
+    defaultLocale: 'catchAll',
+  },
 }
