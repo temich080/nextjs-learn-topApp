ee1d25c3da2f3a280c79c0b982f0abc8192207ce
tarasov <tarasov@reksoft.ru>
2021-08-25 23:39:47 +0300

    NextJS-chapter-11 Добавлен getStaticProps для index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import {HTag, PTag, Button} from "../components";
 import {withLayout} from "../layout/Layout";
+import axios from 'axios';
+import {GetStaticProps} from "next";
+import {MenuItem} from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({menu}: HomeProps): JSX.Element {
+    console.log(menu);
     return (
         <div>
             <Head>
@@ -11,6 +15,9 @@ function Home(): JSX.Element {
             </Head>
 
             <>
+                <ul>
+                    {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+                </ul>
                 <HTag tag={'h1'}>
                     Привет, я в Интернете теперь!!!
                 </HTag>
@@ -46,3 +53,21 @@ function Home(): JSX.Element {
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> =  async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`, {
+        firstCategory
+    })
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    }
+}
+
+interface HomeProps extends Record<string, unknown>{
+    menu: MenuItem[],
+    firstCategory: number
+}
